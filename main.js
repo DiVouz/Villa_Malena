52c97c418c9a85a5da19abe3ce7f4e1e71bcfe53
DiVouz <divouz@outlook.com>
2021-03-05 14:27:00 +0200

    updated images

--- main.js
+++ main.js
@@ -63,11 +63,13 @@ function plusSlides(n) {
 function showSlides(n) {
     var i;
     var x = document.getElementsByClassName(slideId);
-    if (n > x.length) {slideIndex = 1}    
+    if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length}
+
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";  
     }
+    
     x[slideIndex-1].style.display = "block";
 }
 
